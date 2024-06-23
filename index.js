0bcc37cd9c0c611a0583b0c491a932d4c8ad094e
purvi123Jain <158695584+purvi123Jain@users.noreply.github.com>
2024-06-23 20:57:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+// //overlay naam ki koi html class thi usko fetch kara hai querryselector ki help se
+
+// //   Modal open function
+// // css mai modal k andr properties add ki hai
+// const openModal = () => {
+//   console.log("Modal is Open");
+//   modal.classList.add("active");
+//   overlay.classList.add("overlayactive");
+// //pahele overlay ki opacity 0 rakhi thi mtlb vo invisible krdi thi fr baad mai opacity 1 krdi
+// //   overlay active mai opacity 1 krdi  ialiye vo transparent dikhai de raha h ab
+
+// //   pointerevent initial kra hai
+// };
+
+// // Modal close function
+// const closeModal = () => {
+//   modal.classList.remove("active");
+//   overlay.classList.remove("overlayactive");
+// };
